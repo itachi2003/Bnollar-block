acf6af158fcacaaeb44789e43b7dea50b218ffa6
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-19 19:17:11 +0400

    Update bnollar.sol

--- bnollar.sol
+++ bnollar.sol
@@ -1,21 +1,16 @@
-
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract Bnollar is ERC721URIStorage {
 
-    using Counters for Counters.Counter;
+contract Bnollar is ERC721("bnollar", "BN") {
+    uint256 tokenId;
+    tweet[] public tweets;
 
-    Counters.Counter private _tokenIds;
-    uint256 public listPrice = 0.01 ether;
-    address payable owner;
-
-    struct Tweet {
+    struct tweet{
         string name;
         string description;
         uint256 upvotes;
@@ -23,61 +18,17 @@ contract Bnollar is ERC721URIStorage {
         address fromAddress;
     }
 
-    Tweet[] public tweets;
-
-    event TokenListedSuccess (
-        uint256 indexed tokenId,
-        address owner,
-        address seller,
-        uint256 price,
-        bool currentlyListed
-    );
-
-    event TweetCreated(uint256 tokenId, string name, string description);
-
-    constructor() ERC721("Bnollar", "BN") {
-        owner = payable(msg.sender);
-    }
-
-    function updateListPrice(uint256 _listPrice) public payable {
-        require(owner == msg.sender, "Only owner can update listing price");
-        listPrice = _listPrice;
-    }
-
-    function getListPrice() public view returns (uint256) {
-        return listPrice;
-    }
-
-    function createTweet(string memory name, string memory description) public {
-        uint256 tokenId = _tokenIds.current();
-        _tokenIds.increment();
-        _safeMint(msg.sender, tokenId);
-        tweets.push(Tweet({
-            name: name,
-            description: description,
-            upvotes: 0,
-            comments: new string[](0),
-            fromAddress: msg.sender
-        }));
-        emit TweetCreated(tokenId, name, description);
-    }
-
-    function upvote(uint256 tweetIndex) public {
-        require(tweetIndex < tweets.length, "Tweet does not exist");
-        tweets[tweetIndex].upvotes += 1;
-    }
-
-    function addComment(uint256 tweetIndex, string memory comment) public {
-        require(tweetIndex < tweets.length, "Tweet does not exist");
-        tweets[tweetIndex].comments.push(comment);
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(tokenId < tweets.length, "Tweet does not exist");
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
         bytes memory dataURI = abi.encodePacked(
             '{',
-                '"name":', '"', tweets[tokenId].name, '",'  '"description":' , '"',  tweets[tokenId].description, '"', ',' ,
-            '"attributes":', '[', '{', '"trait_type":', '"Upvotes",' , '"value":', Strings.toString(tweets[tokenId].upvotes), '}', ']' , '}'
+                '"name":', '"', tweets[_tokenId].name, '",'  '"description":' , '"',  tweets[_tokenId].description, '"', ',' ,
+
+            '"attributes":', '[', '{', '"trait_type":', '"Upvotes",' , '"value":', Strings.toString(tweets[_tokenId].upvotes), '}', ']' , '}'
         );
 
         return string(
@@ -88,81 +39,27 @@ contract Bnollar is ERC721URIStorage {
         );
     }
 
-    function createListedToken(uint256 tokenId, uint256 price) private {
-        require(msg.value == listPrice, "Incorrect listing price");
-        require(price > 0, "Price must be greater than zero");
-
-        idToListedToken[tokenId] = ListedToken(
-            tokenId,
-            payable(address(this)),
-            payable(msg.sender),
-            price,
-            true
-        );
-  _transfer(msg.sender, address(this), tokenId);
-        emit TokenListedSuccess(
-            tokenId,
-            address(this),
-            msg.sender,
-            price,
-            true
-        );
+    function writeTweet(string memory prefName, string memory prefDesc) public {
+        _safeMint(msg.sender, tokenId);
+        tweets.push(tweet({
+            name: prefName,
+            description: prefDesc,
+            upvotes: 0,
+            comments: new string[](0),
+            fromAddress: msg.sender
+        }));
+        tokenId = tokenId + 1;
     }
 
-    function executeSale(uint256 tokenId) public payable {
-        uint price = idToListedToken[tokenId].price;
-        address seller = idToListedToken[tokenId].seller;
-        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
-
-        idToListedToken[tokenId].currentlyListed = true;
-        idToListedToken[tokenId].seller = payable(msg.sender);
-        _itemsSold.increment();
-
-        _transfer(address(this), msg.sender, tokenId);
-        approve(address(this), tokenId);
-
-        payable(owner).transfer(listPrice);
-        payable(seller).transfer(msg.value);
+    function upvote(uint tweetIndex) public {
+        tweets[tweetIndex].upvotes += 1;
     }
 
-    function getAllNFTs() public view returns (ListedToken[] memory) {
-        uint nftCount = _tokenIds.current();
-        ListedToken[] memory tokens = new ListedToken[](nftCount);
-        uint currentIndex = 0;
-        uint currentId;
-
-        for(uint i=0; i < nftCount; i++) {
-            currentId = i + 1;
-            ListedToken storage currentItem = idToListedToken[currentId];
-            tokens[currentIndex] = currentItem;
-            currentIndex += 1;
-        }
-
-        return tokens;
+    function addComment(uint256 tweetIndex, string memory prefComments) public {
+        tweets[tweetIndex].comments.push(prefComments);
     }
 
-    function getMyNFTs() public view returns (ListedToken[] memory) {
-        uint totalItemCount = _tokenIds.current();
-        uint itemCount = 0;
-        uint currentIndex = 0;
-        uint currentId;
-
-        for(uint i=0; i < totalItemCount; i++) {
-            if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender){
-                itemCount += 1;
-            }
-        }
-
-        ListedToken[] memory items = new ListedToken[](itemCount);
-        for(uint i=0; i < totalItemCount; i++) {
-            if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender) {
-                currentId = i+1;
-                ListedToken storage currentItem = idToListedToken[currentId];
-                items[currentIndex] = currentItem;
-                currentIndex += 1;
-            }
-        }
-
-        return items;
+    function getAllTweets() public view returns(tweet[] memory) {
+        return tweets;
     }
 }
